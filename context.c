3a10a8f3cd62939b2b999f4c98408a5d1442c575
sahrohit <sahrohit9586@gmail.com>
2025-02-17 00:22:18 -0600

    Add .gitignore and refactor context and timer code with comments

--- context.c
+++ context.c
@@ -4,11 +4,15 @@
 #include <ucontext.h>
 #include <errno.h>
 
-#define STACK_SIZE SIGSTKSZ
+// a system default specifying the number of bytes that would be used to cover the usual case when manually allocating an alternate stack area.
+// #define STACK_SIZE SIGSTKSZ // Linter is throwing error for this line
+
+// Alternative based from https://stackoverflow.com/questions/40299849/context-switching-is-makecontext-and-swapcontext-working-here-osx
+#define STACK_SIZE (1 << 15) // 32KB
 
 #define N 5
 
-int which = 0;
+// Initializing an array of ucontext_t structures
 ucontext_t contexts[N];
 
 typedef struct stack
@@ -23,24 +27,39 @@ void func()
 
 int main()
 {
+  // Initializing an array of stack structures
   stack *stacks = (stack *)(malloc(STACK_SIZE * N));
+
+  // Create a parent context to switch from and back to
   ucontext_t parent;
+
   for (int i = 0; i < N; i++)
   {
+    // Get the user context and store it in context[i]
     getcontext(&(contexts[i]));
+
+    // Pointer to the context that will be resumed when the context is terminated
     contexts[i].uc_link = &parent;
+
     contexts[i].uc_stack.ss_sp = &(stacks[i]);
     contexts[i].uc_stack.ss_size = STACK_SIZE;
     contexts[i].uc_stack.ss_flags = 0;
 
+    // Make the context - pass the pointer to the function that needs to be executed
+    // 0 - the third parameter is the number of arguments that the function takes
     makecontext(&(contexts[i]), func, 0);
+
+    // Printing Stanard Error if any error occured
     fprintf(stderr, "made context %d\n", i);
   }
 
   for (int i = 0; i < N; i++)
   {
     printf("%d\n", i);
+
+    // Switching from parent to context[i]
     swapcontext(&parent, &(contexts[i]));
   }
+
   return 0;
 }
--- timer.c
+++ timer.c
@@ -5,24 +5,40 @@
 #include <unistd.h>
 #include <sys/time.h>
 
+// Creating shorthand for itimerval struct
 struct itimerval it;
 
+// Handler when the virtual timer expires
 void signal_handler()
 {
+  // Static to preserve the value of i from being re-assigned
   static int i = 0;
   i += 1;
   printf("(%2d) handled a timer signal, setting timer\n", i);
+
+  // Setting the timer to 500ms
+  // This calls SIGVTALRM signal after 500ms
+  // Hence this again calls this signal_handler function
   setitimer(ITIMER_VIRTUAL, &it, NULL);
 }
 
 int main()
 {
+
+  // Registering the signal handler for SIGVTALRM
   signal(SIGVTALRM, signal_handler);
-  it.it_value.tv_sec = 0;
+
+  // Setting the timer to 500ms
+  it.it_value.tv_sec = 1;
   it.it_value.tv_usec = 500000; // 500ms
+
   setitimer(ITIMER_VIRTUAL, &it, NULL);
+
+  // This loop is for preventing the program from exiting
   while (1)
   {
     // printf("main loop\n");
   }
+
+  return 0;
 }
